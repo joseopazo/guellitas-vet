3e6373d233450809b8ba8297053f451b47bbde27
jose opazo <70711163+joseopazo@users.noreply.github.com>
2026-09-20 14:05:00 -0300

    Añadir el enlace 'Reportes' a menu_clinico.php

--- public/partials/menu_clinico.php
+++ public/partials/menu_clinico.php
@@ -10,6 +10,7 @@
   <nav class="menu-lateral">
     <a href="dashboard_clinico.php" class="<?= $activo === 'inicio' ? 'activo' : '' ?>">Mi agenda</a>
     <a href="mi_horario.php" class="<?= $activo === 'horario' ? 'activo' : '' ?>">Mi disponibilidad</a>
+    <a href="reportes.php" class="<?= $activo === 'reportes' ? 'activo' : '' ?>">Reportes</a>
     <a href="perfil.php" class="<?= $activo === 'perfil' ? 'activo' : '' ?>">Mi perfil</a>
   </nav>
   <div class="contenido">
